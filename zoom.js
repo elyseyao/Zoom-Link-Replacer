42a0021b0594c8b4cd486df84ecf1262e9479f6d
Elyse Yao <elyseyao@gmail.com>
2020-04-02 10:29:20 -0700

    added redirection

--- zoom.js
+++ zoom.js
@@ -1,1 +1,9 @@
-javascript: (function() {  document.body.innerHTML = document.body.innerHTML.replace(/zoom\.us\/[a-zA-Z]*\/([0-9]{9,10})/g, 'zoom.us/wc/$1/join').replace(/(\/join)+/g, '/join');})();
+javascript: (function() {
+    var links = document.links;
+    let template = /(.*)zoom.us\/[a-zA-Z]*\/([0-9]{9,10})/gmi; 
+    for (var i = 0; i < links.length; i++) {
+        links[i].href = links[i].href.replace(template, '$1zoom.us/wc/$2/join');
+    }
+    document.body.innerHTML = document.body.innerHTML.replace(template, '$1zoom.us/wc/$2/join');
+    window.location.replace(/.*zoom.us\/[a-zA-Z]*\/([0-9]{9,10})(.*)/i, '$1zoom.us/wc/$2/join');
+})();
