5e2150ea664eeb3228241bcda7626c7fd0df32ce
Elyse Yao <elyseyao@gmail.com>
2020-04-02 10:33:08 -0700

    updated README.md

--- zoom.js
+++ zoom.js
@@ -1,9 +1,8 @@
 javascript: (function() {
-    var links = document.links;
-    let template = /(.*)zoom.us\/[a-zA-Z]*\/([0-9]{9,10})/gmi; 
-    for (var i = 0; i < links.length; i++) {
-        links[i].href = links[i].href.replace(template, '$1zoom.us/wc/$2/join');
+    document.body.innerHTML = document.body.innerHTML.replace(/zoom\.us\/[a-zA-Z]*\/([0-9]{9,10})/g, 'zoom.us/wc/$1/join').replace(/(\/join)+/g, '/join');
+    if(window.location.href.indexOf("zoom.us")>-1){
+        url = window.location.href.replace(/zoom\.us\/[a-zA-Z]*\/([0-9]{9,10})/g, 'zoom.us/wc/$1/join');
+        window.location.replace(url);
     }
-    document.body.innerHTML = document.body.innerHTML.replace(template, '$1zoom.us/wc/$2/join');
-    window.location.replace(/.*zoom.us\/[a-zA-Z]*\/([0-9]{9,10})(.*)/i, '$1zoom.us/wc/$2/join');
 })();
+
